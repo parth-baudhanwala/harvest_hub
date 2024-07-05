34e529bf73d8526d7be17298896e463892592f6a
Parth Baudhanwala <parthbaudhanwala45@gmail.com>
2024-07-05 23:39:08 +0530

    Create Extension.cs

---
+++ src/BuildingBlocks/BuildingBlocks.MessageBroker/MassTransit/Extension.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.MessageBroker.MassTransit;
+
+public static class Extension
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        services.AddMassTransit(busConfig =>
+        {
+            busConfig.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null) busConfig.AddConsumers(assembly);
+
+            busConfig.UsingRabbitMq((context, rabbitMqConfig) =>
+            {
+                rabbitMqConfig.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:Username"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
+                });
+                rabbitMqConfig.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
