08b45e56d7ea5e228e70e2c52b25d2ac800e5447
Parth Baudhanwala <parthbaudhanwala45@gmail.com>
2024-07-05 23:38:54 +0530

    Implemented Building Block Message Broker

---
+++ src/BuildingBlocks/BuildingBlocks.MessageBroker/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,35 @@
+﻿namespace BuildingBlocks.MessageBroker.Events;
+
+public record BasketCheckoutEvent : IntegrationEvent
+{
+    public string Username { get; set; } = default!;
+    public Guid CustomerId { get; set; } = default!;
+    public decimal TotalPrice { get; set; } = default!;
+
+    // Shipping and BillingAddress
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string EmailAddress { get; set; } = default!;
+    public string AddressLine { get; set; } = default!;
+    public string Country { get; set; } = default!;
+    public string State { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+
+    // Payment
+    public string CardName { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
+    public string Expiration { get; set; } = default!;
+    public string Cvv { get; set; } = default!;
+    public int PaymentMethod { get; set; } = default!;
+
+    // Items
+    public List<BasketCheckoutItem> Items { get; set; } = default!;
+}
+
+public class BasketCheckoutItem
+{
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = default!;
+}
---
+++ src/BuildingBlocks/BuildingBlocks.MessageBroker/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.MessageBroker.Events;
+
+public record IntegrationEvent
+{
+    public static Guid Id => Guid.NewGuid();
+    public static DateTime OccurredOn => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
